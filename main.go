319c7237cf9302dffd0c92bc455d0ea4fe37b867
Patrick Gorospe <pgorospe@deloitte.com>
2019-01-26 14:13:26 -0500

    Implemented api in go

--- main.go
+++ main.go
@@ -4,9 +4,13 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
+	"os"
+	"strconv"
 
 	"github.com/gorilla/mux"
 )
@@ -16,17 +20,87 @@ import (
 // and copious amounts of caffeine. Despite their oddities, the
 // Hacker is a loving, welcoming, and friendly kind.
 type Hacker struct {
-	// This data structure is sad!
+	Name             string `json:"name"`
+	ID               int    `json:"id"`
+	FavoriteLanguage string `json:"favorite-language"`
+}
+
+// Hackers is a struct which contains
+// an array of Hacker
+type Hackers struct {
+	Hackers []Hacker `json:"hackers"`
+}
+
+// getHackers reads a local JSON file and returns all hackers
+func getHackers() Hackers {
+	// Open our json
+	jsonFile, err := os.Open("hackers.json")
+	// if we os.Open returns an error then handle it
+	if err != nil {
+		fmt.Println(err)
+	}
+	// defer the closing of our jsonFile so that we can parse it later on
+	defer jsonFile.Close()
+
+	fmt.Println("Successfully Opened hackers.json")
+
+	// Declare our slice of hackers
+	var hackers Hackers
+
+	// read our json file into a byte array
+	// _ means we are intentionally ignoring the err return value -> Not best	// defer the closing of our jsonFile so that we can parse it later on
+	defer jsonFile.Close()
+
+	b, _ := ioutil.ReadAll(jsonFile)
+
+	// Unmarshal b into hackers,defined above
+	// Notice the pointer &
+	json.Unmarshal(b, &hackers)
+
+	fmt.Println(hackers)
+
+	return hackers
 }
 
 // GetAllHackers basic GET api endpoint
 func GetAllHackers(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "GetAllHackers is not implemented yet !")
+	hackers := getHackers()
+
+	jsonResponse, err := json.Marshal(hackers)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(jsonResponse)
 }
 
 // GetHackerByID basic GET api endpoint
 func GetHackerByID(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "GetHackerByID is not implemented yet !")
+	// Multiple steps in one! We are:
+	// 0.) Using the r Request objects to get more information about our request
+	// 1.) Getting a map from mux that represents the query parameters
+	// 2.) Calling the specific key/value pair where key is "id"
+	// 3.) Converting the value from a string to integer so we can compare with the Hacker.ID
+	id, _ := strconv.Atoi(mux.Vars(r)["id"])
+
+	hackers := getHackers()
+
+	for _, hacker := range hackers.Hackers {
+		if hacker.ID == id {
+			jsonResponse, err := json.Marshal(hacker)
+			if err != nil {
+				w.WriteHeader(http.StatusInternalServerError)
+			}
+			w.Write(jsonResponse)
+			return
+		}
+	}
+	// Tell our user this hacker DNE in our db!
+	w.WriteHeader(http.StatusNotFound)
+	w.Write([]byte(fmt.Sprintf("Cannot find hacker of id: %v", id)))
 }
 
 func main() {
